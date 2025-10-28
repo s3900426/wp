c533bcb123c20dffe3080e27c6b424c6b62b3d7c
s3900426 <s3900426@student.rmit.edu.au>
2025-10-28 22:25:46 +1100

    working gallery now in javascript, beginning register function

--- a2/gallery.php
+++ a2/gallery.php
@@ -8,19 +8,18 @@ $sql = "SELECT skill_id, title, image_path,category FROM skills ORDER BY skill_i
 $sqlCat = "SELECT DISTINCT category FROM skills ORDER BY created_at";
 $records = $conn->query($sql);
 $recordsCat = $conn->query($sqlCat);
-$currentCat = isset($_POST['category']) && $_POST['category'] != '' ? $_POST['category']:'none';
 ?>
 
 <main class="container">
     <h1>Skills Gallery</h1>
-    <form method="post" action="" class="" enctype="multipart/form-data" id="categorySelect">
+    <form onchange="" class="" enctype="multipart/form-data" id="categorySelect">
         <div class="mb-3">
         <label for="category" class="form-label">Filter by category</label>
-        <select class="form-select form-control w-25" id="category" name="category" onchange="this.form.submit()">
+        <select class="form-select form-control w-25" id="category" name="category">
             <?php
-            echo'<option value="none" '.($currentCat == $row['category'] ? 'selected' : '').'>None</option>';
+            echo'<option value="none">None</option>';
             foreach ($recordsCat as $row) {
-                echo '<option value="'.$row['category'].'" '.($currentCat == $row['category'] ? 'selected' : '').'>'.$row['category'].'</option>';
+                echo '<option value="'.$row['category'].'">'.$row['category'].'</option>';
             }
             ?>
         </select>
@@ -29,24 +28,12 @@ $currentCat = isset($_POST['category']) && $_POST['category'] != '' ? $_POST['ca
     
     <div class="row">
         <?php
-        if ($currentCat == 'none') {
             foreach ($records as $row) {
-                echo '<div class="col-6 col-md-3 p-2">';
-                echo '<img class="img-fluid gallery rounded" src="' . $row['image_path'] . '" alt="' . $row['title'] . '" data-bs-toggle="modal" data-bs-target="#imageModal">';
-                echo ' <p><a href="details.php?skill_id=' . $row['skill_id'] . '">' . $row['title'] . '</a></p>';
-                echo '</div>';
-            }
-        } else{
-            foreach ($records as $row) {
-                if ($row['category'] == $currentCat) {
-                    echo '<div class="col-6 col-md-3 p-2">';
+                    echo '<div class="col-6 col-md-3 p-2 '.$row['category'].' galdiv">';
                     echo '<img class="img-fluid gallery rounded" src="' . $row['image_path'] . '" alt="' . $row['title'] . '" data-bs-toggle="modal" data-bs-target="#imageModal">';
                     echo ' <p><a href="details.php?skill_id=' . $row['skill_id'] . '">' . $row['title'] . '</a></p>';
                     echo '</div>';
-                }
             }
-
-        }
 
         ?>
         <div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true">
--- a2/register.php
+++ a2/register.php
@@ -1,3 +1,35 @@
-<?php 
-
+<?php
+$title = 'Login';
+include('includes/db_connect.inc');
+include('includes/header.inc');
+include('includes/nav.inc');
+session_start();
 ?>
+<main class="container col-12">
+    <h1 class="p-10">Register</h1>
+    <form method="post" action="process_register.php" class="form-control" enctype="multipart/form-data"
+        id="registerForm">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email <span class="orangeAsterisk">*</span></label>
+            <input class="form-control" type="email" id="email" name="email" placeholder="Enter Email"
+                required>
+        </div>
+        <div class="mb-3">
+            <label for="username" class="form-label">Username <span class="orangeAsterisk">*</span></label>
+            <input class="form-control" type="username" id="username" name="username" placeholder="Enter Username"
+                required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password <span class="orangeAsterisk">*</span></label>
+            <input class="form-control" type="password" id="password" name="password" placeholder="Enter Password"
+                required>
+        </div>
+        <button type="submit" class="btn-dark btn cont rounded-5 skill">Submit</button>
+    </form>
+
+</main>
+
+<?php
+include('includes/footer.inc')
+
+    ?>
